c1db623d8fd7be9629ee7281b9cf305b83dd8ea8
kishidashota <shidanemoaki@gmail.com>
2020-05-09 19:05:34 +0900

    ショップ登録フォーム完成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,15 @@
 class ShopsController < ApplicationController
   def new
+    @shop = Shop.new
   end
+
+  def create
+    Shop.create(shop_params)
+  end
+
+  private
+  def shop_params
+    params.require(:shop)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "tops#index"
   resources :questions, only: [:new, :show]
   devise_scope :user do
-    resources :shops, only: [:new, :show]
+    resources :shops, only: [:new, :show,:create]
     resources :addresses, only: [:new, :show]
   end
 end
